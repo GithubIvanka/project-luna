1289f27a08d68b105a36a71a75755515ce5fe251
Anton Frolov <114659562+GithubIvanka@users.noreply.github.com>
2026-08-27 09:10:52 +0700

    refactor: tighten luna-common Version value type

--- components/luna-common/src/version.rs
+++ components/luna-common/src/version.rs
@@ -1,17 +1,29 @@
 use std::fmt;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+/// Semantic version value used by Luna's foundational APIs.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct Version {
-    pub major: u32,
-    pub minor: u32,
-    pub patch: u32,
+    major: u32,
+    minor: u32,
+    patch: u32,
 }
 
 impl Version {
-    pub fn new(major: u32, minor: u32, patch: u32) -> Self {
-        Self {
-            major, minor, patch,
-        }
+    /// Creates a semantic version.
+    pub const fn new(major: u32, minor: u32, patch: u32) -> Self {
+        Self { major, minor, patch }
+    }
+
+    pub const fn major(self) -> u32 {
+        self.major
+    }
+
+    pub const fn minor(self) -> u32 {
+        self.minor
+    }
+
+    pub const fn patch(self) -> u32 {
+        self.patch
     }
 }
 
