a4a1b35abdad31116400570c28cb18b53ae36f71
Anton Frolov <114659562+GithubIvanka@users.noreply.github.com>
2026-08-27 12:40:01 +0700

    test: define luna-common version behavior

--- components/luna-common/src/version.rs
+++ components/luna-common/src/version.rs
@@ -32,3 +32,24 @@ impl fmt::Display for Version {
         write!(f, "{}.{}.{}", self.major, self.minor, self.patch)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Version;
+
+    #[test]
+    fn version_exposes_components_and_formats_stably() {
+        let version = Version::new(1, 6, 0);
+
+        assert_eq!(version.major(), 1);
+        assert_eq!(version.minor(), 6);
+        assert_eq!(version.patch(), 0);
+        assert_eq!(version.to_string(), "1.6.0");
+    }
+
+    #[test]
+    fn versions_have_value_semantics() {
+        assert!(Version::new(1, 2, 0) < Version::new(1, 3, 0));
+        assert_eq!(Version::new(2, 0, 0), Version::new(2, 0, 0));
+    }
+}
