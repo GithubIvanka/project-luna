dea0795fa6e2baa9bb2a7b5f4f6c5d5b06da31cf
Anton Frolov <114659562+GithubIvanka@users.noreply.github.com>
2026-09-08 21:25:23 +0700

    Carry manifest and kernel identities into boot targets

--- boot/luna-boot/src/target.rs
+++ boot/luna-boot/src/target.rs
@@ -6,10 +6,13 @@ use alloc::vec::Vec;
 #[derive(Debug, Clone)]
 pub struct BootTarget {
     pub name: String,
+    pub image_family: String,
     pub system_version: String,
     pub system_image_path: String,
+    pub manifest_path: String,
     pub init_path: String,
     pub kernel_path: String,
+    pub kernel_id: String,
     pub kernel_cmdline: String,
     pub is_recovery: bool,
     pub is_factory: bool,
@@ -18,17 +21,23 @@ pub struct BootTarget {
 impl BootTarget {
     pub fn new(
         name: impl Into<String>,
+        image_family: impl Into<String>,
         system_version: impl Into<String>,
         system_image_path: impl Into<String>,
+        manifest_path: impl Into<String>,
         init_path: impl Into<String>,
         kernel_path: impl Into<String>,
+        kernel_id: impl Into<String>,
     ) -> Self {
         Self {
             name: name.into(),
+            image_family: image_family.into(),
             system_version: system_version.into(),
             system_image_path: system_image_path.into(),
+            manifest_path: manifest_path.into(),
             init_path: init_path.into(),
             kernel_path: kernel_path.into(),
+            kernel_id: kernel_id.into(),
             kernel_cmdline: String::new(),
             is_recovery: false,
             is_factory: false,
